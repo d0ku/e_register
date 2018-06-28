f6fd81b7e7c94f1b75f1f263e3f0381ca9adf963
d0ku <darthtyranus666666@gmail.com>
2018-06-28 02:41:23 +0200

    pure chaos

--- core/core.go
+++ core/core.go
@@ -2,20 +2,83 @@ package core
 
 import (
 	"fmt"
-	"io"
+	"html/template"
+	"io/ioutil"
 	"net/http"
 )
 
 func handler(response http.ResponseWriter, request *http.Request) {
-	//	fmt.Println(request.TLS)
-	io.WriteString(response, "Hello World!")
+	//That implementation is not stable and dangerous, but fun so I will keep it for now.
+
+	//If address is "/" point to index, else try to find adequate file in page folder.
+
+	if request.Method == "GET" {
+		url := request.URL
+		var path string
+
+		var pageBaseAddress = "./page/"
+
+		if url.String() == "/" {
+			path = pageBaseAddress + "index.html"
+
+		} else {
+			path = pageBaseAddress + url.Path[1:]
+		}
+
+		fileContent, err := ioutil.ReadFile(path)
+
+		if err != nil {
+			//Write error html content to response and return it.
+			fmt.Println(err)
+
+			page := GetPage()
+			page.SetAuthor("d0ku")
+			page.SetTitle("File Not Found!")
+			page.SetDescription("Displays when file could not be found.")
+			page.AddCSSFile("base.css")
+
+			page.SetBody("<h1 class=\"warning\">FILE NOT FOUND!</h1>")
+
+			errorContent := []byte(page.GetHTMLString())
+
+			response.Write(errorContent)
+			return
+		}
+		//Set correct Content-Type in response basing on request.
+
+		response.Header().Set("Content-Type", request.Header.Get("Content-Type"))
+		response.Write(fileContent)
+	} else {
+		//POST
+		//call some functinos or do something
+	}
 }
 
+func loginHandler(response http.ResponseWriter, request *http.Request) {
+
+	fmt.Println(request.Method)
+	if request.Method == "GET" {
+		t, _ := template.ParseFiles("page/login.gtpl")
+		t.Execute(response, nil)
+	} else {
+		request.ParseForm()
+		//Validate data, and check whether it can be used to log into database.
+		fmt.Println(request.Form["username"])
+	}
+}
+
+func deleteHandler(response http.ResponseWriter, request *http.Request) {
+	response.Write([]byte(request.URL.String()))
+}
+
+//Run starts the server at default port and prints info in console how to connect to it.
 func Run() {
 	var port = "1234"
 
 	fmt.Println("Listen to me at: https://localhost:" + port)
 
+	http.HandleFunc("/login", loginHandler)
+	http.HandleFunc("/delete/", deleteHandler)
 	http.HandleFunc("/", handler)
 	err := http.ListenAndServeTLS(":"+port, "server.crt", "server.key", nil)
 
---
+++ page/login.gtpl
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>Login Page</title>
+    <link rel="stylesheet" type="text/css" href="CSS/reset.css">
+    </head>
+    <body>
+        <form action="/login" method="post">
+            Username:<input type="text" name="username">
+            Pasd:<input type="password" name="password">
+            <input type="submit" value="Login">
+        </form>
+    </body>
+</html>
