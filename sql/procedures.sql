d927a43b68da68114f5ad882a0d450efb088be41
d0ku <darthtyranus666666@gmail.com>
2018-07-11 01:07:11 +0200

    check_login_data and add_user rewritten to meet UML diagram requirements

--- sql/procedures.sql
+++ sql/procedures.sql
@@ -1,13 +1,42 @@
-CREATE OR REPLACE FUNCTION add_user(IN username text, password text) RETURNS VOID AS $$
+CREATE OR REPLACE FUNCTION add_user(IN user_name text, password text, usertype user_type, final_id integer) RETURNS BOOLEAN AS $$
 DECLARE
+temp integer;
 BEGIN
-    INSERT INTO Users("username","password") VALUES(username, crypt(password, gen_salt('bf',8)));
+    --Lock should prevent race condition (both connections check that user does not exist, then one can add and second can't add.).
+    LOCK Users IN EXCLUSIVE MODE;
+    IF EXISTS(  SELECT Users.username
+        FROM Users
+        WHERE Users.username=user_name
+        )
+        THEN
+        -- could not create user, because user with such username already exists.
+        RETURN FALSE;
+    ELSE
+        --user can be created.
+        INSERT INTO Users("username", "hashed_password", "user_type", "final_id") VALUES(user_name, crypt(password, gen_salt('bf',8)), usertype, final_id);
+        RETURN TRUE;
+    END IF;
 END
 $$ language plpgsql;
 
-CREATE OR REPLACE FUNCTION check_login_data(IN provided_username text, provided_password text) RETURNS text AS $$
+CREATE TYPE user_data AS (user_exists BOOLEAN, user_type user_type, id integer);
+
+CREATE OR REPLACE FUNCTION check_login_data(IN provided_username text, provided_password text) RETURNS user_data AS $$
 DECLARE
+return_data user_data;
 BEGIN
-   return (SELECT username FROM Users WHERE username = provided_username AND password = crypt(provided_password, password));
+    SELECT user_type, final_id
+    INTO return_data.user_type, return_data.id
+    FROM Users
+    WHERE username=provided_username
+    AND hashed_password = crypt(provided_password, hashed_password);
+    return_data.user_exists=true;
+
+    IF return_data.id IS NULL OR return_data.user_type IS NULL
+        THEN
+        return_data.user_exists=false;
+    END IF;
+
+    RETURN return_data;
 END
 $$ language plpgsql;
