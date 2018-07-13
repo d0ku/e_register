5d8be54effcb3c7d79f3872050fff2c6a43cfb73
d0ku <darthtyranus666666@gmail.com>
2018-07-13 18:40:34 +0200

    some functions added to sql files, base for future tests written in sql/test_commands.sql

--- sql/procedures.sql
+++ sql/procedures.sql
@@ -3,7 +3,6 @@ DECLARE
 temp integer;
 BEGIN
     --Lock should prevent race condition (both connections check that user does not exist, then one can add and second can't add.).
-    -- Possible BUG. Rethink is needed.
     LOCK Users IN EXCLUSIVE MODE;
     IF EXISTS(  SELECT Users.username
         FROM Users
@@ -31,6 +30,7 @@ BEGIN
     FROM Users
     WHERE username=provided_username
     AND hashed_password = crypt(provided_password, hashed_password);
+
     return_data.user_exists=true;
 
     IF return_data.id IS NULL OR return_data.user_type IS NULL
@@ -44,9 +44,137 @@ $$ language plpgsql;
 
 CREATE OR REPLACE FUNCTION add_semester(IN sem_type semester_type, year integer) RETURNS integer AS $$
 DECLARE
+--return id of semester, -1 if can't be added.
     to_return integer;
 BEGIN
     INSERT INTO Semesters("semester", "year") VALUES(sem_type, year) RETURNING id_semester INTO to_return;
     RETURN to_return;
+    EXCEPTION WHEN integrity_constraint_violation THEN
+        RETURN -1;
+END
+$$ language plpgsql;
+
+
+CREATE OR REPLACE FUNCTION add_student(IN school_id integer, class_id integer, address_id integer, forename_input text, surname_input text, sex_input sex_type, born_date DATE) RETURNS integer AS $$
+--If student was successfully added -> return his id, else return -1 (-1 as response from that function means there was some error in the way);
+DECLARE
+    to_return integer;
+BEGIN
+    -- TODO: test it thoroughly, when done with add_address, add_class, add_school,.
+-- SELECT add_student(1,1,1,'testFore','testSure','male','Jan-08-1999');
+    INSERT INTO Students("id_school","id_class","id_address","forename","surename","sex","date_of_birth") VALUES(school_id, class_id, address_id, forename_input, surname_input, sex_input, born_date) RETURNING id_student INTO to_return;
+    RETURN to_return;
+    EXCEPTION WHEN integrity_constraint_violation THEN
+        RETURN -1;
+END
+$$ language plpgsql;
+
+
+CREATE OR REPLACE FUNCTION get_address_id(IN city_in text, street_in text, house_number_in text, flat_number_in text, postal_code_in text) RETURNS integer AS $$
+--It creates address if it does not already exist, if it exists it returns id.
+--If address was successfully added -> return his id, else return -1 (-1 as response from that function means there was some error in the way);
+DECLARE
+    to_return integer;
+BEGIN
+    LOCK Addresses IN EXCLUSIVE MODE;
+    IF EXISTS( SELECT id_address 
+        FROM Addresses
+        WHERE city=city_in AND
+            street=street_in AND
+            house_number=house_number_in AND
+            flat_number=flat_number_in AND
+            postal_code=postal_code_in
+        )
+        THEN
+        --return id of address that exists.
+        SELECT id_address
+        INTO to_return
+        FROM Addresses
+        WHERE city=city_in AND
+            street=street_in AND
+            house_number=house_number_in AND
+            flat_number=flat_number_in AND
+            postal_code=postal_code_in;
+        RETURN to_return;
+
+    ELSE
+        --address has to be added.
+        INSERT INTO Addresses("city","street","house_number","flat_number","postal_code") VALUES(city_in, street_in, house_number_in, flat_number_in, postal_code_in) RETURNING id_address INTO to_return;
+        RETURN to_return;
+    END IF;
+END
+$$ language plpgsql;
+
+CREATE OR REPLACE FUNCTION add_subject(IN name text) RETURNS integer AS $$
+DECLARE
+    to_return integer;
+BEGIN
+    --try to add subject.
+    INSERT INTO Subjects("name") VALUES(name) RETURNING id_subject INTO to_return;
+    RETURN to_return;
+    --such subject can't be added, return -1.
+    EXCEPTION WHEN integrity_constraint_violation THEN
+        RETURN -1;
+END
+$$ language plpgsql;
+
+CREATE OR REPLACE FUNCTION add_school(IN name_in text, address_id integer, school_type school_type, bought_package offer_type ) RETURNS integer AS $$
+-- return id of created school if can be created, -1 otherwise.
+DECLARE
+    to_return integer;
+BEGIN
+    --try to add school.
+    INSERT INTO Schools("full_name","id_address","typ","bought_offer") VALUES(name_in, address_id, school_type, bought_package) RETURNING id_school INTO to_return;
+    RETURN to_return;
+    --such subject can't be added, return -1.
+    EXCEPTION WHEN integrity_constraint_violation THEN
+        RETURN -1;
+END
+$$ language plpgsql;
+
+CREATE OR REPLACE FUNCTION add_school_admin(IN teacher_id integer, school_id integer) RETURNS BOOLEAN AS $$
+--return true if successfully added, false otherwise.
+DECLARE
+BEGIN
+    --try to add school admin.
+    INSERT INTO SchoolsAdministrators("id_teacher","id_school") VALUES(teacher_id, school_id);
+    RETURN TRUE;
+    --if there is an error, return false.
+    EXCEPTION WHEN integrity_constraint_violation THEN
+        RETURN FALSE;
+END
+$$ language plpgsql;
+
+CREATE OR REPLACE FUNCTION add_teacher(IN school_id integer, address_id integer, forename text, surename text, degree teacher_rank, sex_in sex_type, born_date DATE) RETURNS INTEGER AS $$
+--Return id if succesfully added, -1 otherwise.
+DECLARE
+    to_return integer;
+BEGIN
+    --try to add school admin.
+    INSERT INTO Teachers("id_school", "id_address", "forename", "surename", "teacher_rank", "sex", "date_of_birth") VALUES(school_id, address_id, forename, surename, degree, sex_in, born_date) RETURNING id_teacher INTO to_return;
+    RETURN to_return;
+    --if there is an error, return -1.
+    EXCEPTION WHEN integrity_constraint_violation THEN
+        RETURN -1;
+END
+$$ language plpgsql;
+
+CREATE OR REPLACE FUNCTION add_grade(IN student_id integer, teacher_id integer, subject_id integer, grade grade_type) RETURNS BOOLEAN AS $$
+BEGIN
+    INSERT INTO Grades("id_student","id_teacher","id_subject","grade") VALUES(student_id, teacher_id, subject_id, grade);
+    RETURN TRUE;
+    EXCEPTION WHEN integrity_constraint_violation THEN
+        RETURN FALSE;
+END
+$$ language plpgsql;
+
+CREATE OR REPLACE FUNCTION add_class(IN school_id integer, teacher_id integer, start_date DATE, end_date DATE, char_in text, num_in integer) RETURNS INTEGER AS $$
+DECLARE
+    to_return integer;
+BEGIN
+    INSERT INTO Classes("id_school","id_educator","start_date","end_date","letter","class_level") VALUES(school_id, teacher_id, start_date, end_date, char_in, num_in) RETURNING id_class INTO to_return;
+    RETURN to_return;
+    EXCEPTION WHEN integrity_constraint_violation THEN
+        RETURN -1;
 END
 $$ language plpgsql;
---
+++ sql/test_commands.sql
@@ -0,0 +1,49 @@
+SELECT add_user('testUser','testPassword','teacher',1); --should return true (correct)
+SELECT add_user('testUser','testPassword','teacher',1); --should return false (duplicate)
+
+
+SELECT check_login_data('testUser','testPassword'); --should return true,teacher,1
+SELECT check_login_data('testUserFailed','testPassword'); --should return false,-,-
+
+SELECT add_semester('summer',2018); --should return 1 (correct)
+SELECT add_semester('winter',2018); --should return 2 (correct)
+SELECT add_semester('winter',2018); --should return -1 (duplicate)
+
+SELECT get_address_id('Paradise City','Imagined','12a','3','01-231'); --should return 1 (correct, create)
+SELECT get_address_id('Paradise City','Imagined','12a','3','01-232'); --should return 2 (correct, create)
+SELECT get_address_id('Paradise City','Imagined','12a','3','01-231'); --should return 1 (correct, find)
+
+SELECT add_subject('Science'); --should return 1 (correct)
+SELECT add_subject('Math'); --should return 2 (correct)
+SELECT add_subject('Science'); --should return -1 (duplicate)
+
+SELECT add_school('dummySchool',1,'LO','standard'); --should return 1 (correct)
+SELECT add_school('dummySchool',1,'TECH','standard'); --should return 2 (correct)
+SELECT add_school('dummySchool',1,'LO','standard'); --should return -1 (duplicate)
+
+SELECT add_teacher(1,1,'tempFor','tempSur','mgr.','male','Jan-18-1923'); --should return 1 (correct)
+SELECT add_teacher(1,1,'tempFor','tempSur','mgr.','male','Jan-18-1924'); --should return 2 (correct)
+SELECT add_teacher(1,1,'tempFor','tempSur','mgr.','male','Jan-18-1923'); -- should return -1 (duplicate)
+
+SELECT add_school_admin(1,1); --should return t (existing teacher to existing school)
+SELECT add_school_admin(1,1); --should return f (duplicate)
+SELECT add_school_admin(1,23); --should return f (existing teacher to not existing school)
+SELECT add_school_admin(23, 1); --should return f (not-existing teacher to existing school)
+SELECT add_school_admin(23, 23); --should return f (not-existing teacher to not-existing school)
+
+SELECT add_class(1,1,'Oct-1-2007','Oct-1-2010','a',1); --should return 1 (correct)
+SELECT add_class(1,1,'Oct-1-2009','Oct-1-2012','c',1); --should return 1 (correct)
+SELECT add_class(1,1,'Oct-1-2007','Oct-1-2010','a',1); --should return -1 (duplicate)
+
+SELECT add_student(1,1,1,'John','Snow','male','Jan-12-1998'); --should return 1 (correct)
+SELECT add_student(1,1,1,'John','Snow','male','Jan-12-1998'); --should return 2 (correct, two students in same class with same names and same born_date can happen)
+SELECT add_student(23,1,1,'Felicia','Snow','female','Jan-12-1998'); --should return -1 (non-existing school)
+SELECT add_student(1,23,1,'Felicia','Snow','female','Jan-12-1998'); --should return -1 (non-existing class)
+SELECT add_student(1,1,23,'Felicia','Snow','female','Jan-12-1998'); --should return -1 (non-existing address)
+
+SELECT add_grade(1,1,1,'4.0'); --should return t (correct)
+SELECT add_grade(1,1,8,'3.0'); --should return f (non-existing subject)
+SELECT add_grade(1,8,1,'3.0'); --should return f (non-existing teacher)
+SELECT add_grade(8,1,1,'3.0'); --should return f (non-existing student)
+
+--TODO: there is a potential for UczniowieLekcje table where uczniowie can be assigned lessons indivdiually (e.g. language lessons where classes consist of people frmo many classes).
