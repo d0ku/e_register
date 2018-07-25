96ca6a5bee900fb9dc917a835bed033482d23bc0
d0ku <darthtyranus666666@gmail.com>
2018-07-26 01:24:45 +0200

    application is now configured with properly formatted config.cfg file

--- main.go
+++ main.go
@@ -1,9 +1,34 @@
 package main
 
-import "github.com/d0ku/database_project_go/core"
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+
+	"github.com/d0ku/database_project_go/core"
+)
 
 func main() {
+	//read config.cfg, parse it and run server adequately
 	//TODO: when app is about to be deployed, that config line should be changed.
-	core.Initialize("d0ku", "test_database", "./page/")
-	core.Run("1234")
+	content, err := ioutil.ReadFile("config.cfg")
+	if err != nil {
+		panic(err)
+	}
+
+	lines := strings.Split(string(content), "\n")
+	lines = lines[0 : len(lines)-1]
+
+	config := make(map[string]string)
+
+	for _, line := range lines {
+		fmt.Println(line)
+		lineElems := strings.Fields(line)
+		config[lineElems[0]] = lineElems[1]
+	}
+
+	fmt.Println(config)
+
+	core.Initialize(config["username"], config["database_name"], config["html_templates_path"])
+	core.Run(config["port"])
 }
