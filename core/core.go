e6282cc67cc6a8562b319ff2ee6e0883bde8bdf6
d0ku <darthtyranus666666@gmail.com>
2018-07-31 16:30:35 +0200

    loginTimeoutsController moved to separate file in core/sessions package.

--- core/core.go
+++ core/core.go
@@ -11,7 +11,6 @@ import (
 	"html/template"
 	"io/ioutil"
 	"log"
-	"math"
 	"net/http"
 	"regexp"
 	"strings"
@@ -150,14 +149,7 @@ func loginHandlerGET(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/main", http.StatusSeeOther)
 }
 
-type UserLoginTry struct {
-	UserType   string
-	UserName   string
-	Timeout    int
-	HasTimeout bool
-}
-
-func loginHandlerDecorator(cookieLifeTime time.Duration, loginTriesController *LoginTriesController) func(http.ResponseWriter, *http.Request) {
+func loginHandlerDecorator(cookieLifeTime time.Duration, loginTriesController *sessions.LoginTriesController) func(http.ResponseWriter, *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == "GET" {
 			loginHandlerGET(w, r)
@@ -175,7 +167,7 @@ func loginHandlerDecorator(cookieLifeTime time.Duration, loginTriesController *L
 
 			var checkSchool bool
 
-			userLoginTry := &UserLoginTry{UserType: userType, UserName: username, Timeout: 0, HasTimeout: false}
+			userLoginTry := &sessions.UserLoginTry{UserType: userType, UserName: username, Timeout: 0, HasTimeout: false}
 			fmt.Println(userLoginTry)
 			if userType == "schoolAdmin" {
 				userType = "teacher"
@@ -247,85 +239,6 @@ func loginHandlerDecorator(cookieLifeTime time.Duration, loginTriesController *L
 	}
 }
 
-//TODO: consider moving below code to another source file.
-//Is this memory safe to never purge addresses which didn't login successfully?
-
-//LoginTriesController enables application to easily control how many times user tried to log in and give him specific timeouts.
-type LoginTriesController struct {
-	tries      map[string]int
-	timeoutEnd map[string]time.Time
-}
-
-func (controller *LoginTriesController) setTimeout(origin string) {
-	var addTime time.Duration
-	tries := controller.tries[origin]
-
-	//TODO: define better policy for timeouts.
-	if tries >= 100 {
-		addTime = time.Second * 30
-	} else if tries >= 50 {
-		addTime = time.Second * 20
-	} else if tries >= 10 {
-		addTime = time.Second * 15
-	} else if tries >= 5 {
-		addTime = time.Second * 10
-	}
-
-	controller.timeoutEnd[origin] = time.Now().Add(addTime)
-}
-
-//AddTry add one try to user, if tries count exceeds specified value, it calls setTimeout function which set timeouts according to specified policy.
-func (controller *LoginTriesController) AddTry(origin string) {
-	_, ok := controller.timeoutEnd[origin]
-	if ok {
-		//Don't do anything if there is a timeout already.
-		return
-	}
-	_, ok = controller.tries[origin]
-
-	if !ok {
-		controller.tries[origin] = 0
-	}
-
-	controller.tries[origin]++
-
-	if controller.tries[origin] >= 5 {
-		controller.setTimeout(origin)
-	}
-}
-
-//ResetTries resets try counter.
-func (controller *LoginTriesController) ResetTries(origin string) {
-	delete(controller.tries, origin)
-}
-
-//GetTimeoutLeft returns 0 if there is no timeout left, or int (seconds) representing how long user has to wait.
-//If timeout left is equal to 0, user can try to log in.
-//Else function should return timeout left (in seconds).
-func (controller *LoginTriesController) GetTimeoutLeft(origin string) int {
-	timeout, ok := controller.timeoutEnd[origin]
-
-	if !ok {
-		//There is no timeout set.
-		return 0
-	}
-
-	if time.Now().After(timeout) {
-		//Timeout already passed, can be deleted.
-		delete(controller.timeoutEnd, origin)
-		return 0
-	}
-
-	//Return time left.
-	timeLeft := int(math.Round(timeout.Sub(time.Now()).Seconds()))
-	if timeLeft == 0 {
-		delete(controller.timeoutEnd, origin)
-		return 0
-	}
-
-	return timeLeft
-}
-
 func deleteHandler(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte(r.URL.String()))
 }
@@ -389,7 +302,7 @@ func Initialize(databaseUser string, databaseName string, templatesPath string, 
 
 	//TODO: some kind of login panel, where admin can add new users?
 
-	loginController := &LoginTriesController{make(map[string]int), make(map[string]time.Time)}
+	loginController := sessions.GetLoginTriesController()
 
 	http.HandleFunc("/login", loginHandlerDecorator(cookieLifeTime, loginController))
 	http.HandleFunc("/logout", redirectWithErrorToLogin(http.HandlerFunc(logoutHandler)))
