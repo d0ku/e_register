c4d4152097af5269980bcea12b5746db3b5d13ac
d0ku <darthtyranus666666@gmail.com>
2018-08-03 18:45:29 +0200

    first create everything and after this give permissions to user

--- sql/setUp.sh
+++ sql/setUp.sh
@@ -16,18 +16,6 @@ fi
 # Create database with provided name as superuser.
 psql -c 'CREATE DATABASE '"$1"';' -U postgres
 
-# Create database user. (only if both password and username are present)
-if [ "$3" != "" ]
-then
-    if [ "$4" != "" ]
-    then
-        psql -c "CREATE USER "$3" password '"$4"'" -U postgres
-        # TODO: This could and should be changed later to give only REALLY necessary permissions.
-        psql -d $1 -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO "$3";" -U postgres
-        psql -d $1 -c "GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO "$3";" -U postgres
-    fi
-fi
-
 # Run all configuration files on previously created database.
 psql -d $1 -f ./extensions.sql -U postgres
 psql -d $1 -f ./create_tables.sql -U postgres
@@ -38,5 +26,17 @@ then
     if [ "$2" == "1" ]
     then
         psql -d $1 -f ./test_commands.sql -U postgres
+    fi
+fi
+
+# Create database user and give him permissions. (only if both password and username are present)
+if [ "$3" != "" ]
+then
+    if [ "$4" != "" ]
+    then
+        psql -c "CREATE USER "$3" password '"$4"'" -U postgres
+        # TODO: This could and should be changed later to give only REALLY necessary permissions.
+        psql -d $1 -c "GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO "$3";" -U postgres
+        psql -d $1 -c "GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO "$3";" -U postgres
     fi
 fi
